06d8e6862d39de610d3ce1079a3da780763890d3
moulik.khamaru <moulik.khamaru@cognizant.com>
2025-11-20 08:03:47 +0000

    modifying template

--- pocketbase/idx-template.nix
+++ pocketbase/idx-template.nix
@@ -4,20 +4,15 @@
     pkgs.go
   ];
   bootstrap = ''
-    # Create output directory and immediately grant write permissions
     mkdir "$out"
     chmod +w "$out"
 
-    # Copy the entire contents of the 'dev' directory.
     cp -r ${./dev}/. "$out"
 
-    # Recursively grant write permissions to all copied files and directories.
     chmod -R +w "$out"
 
-    # Change into the new project directory.
     cd "$out"
 
-    # Tidy the dependencies.
     go mod tidy
   '';
 }
