44690935b55c5f0c4eed2e504d6e6c709f4e69ad
richa.vijayvargiya <richa.vijayvargiya@cognizant.com>
2025-11-27 17:12:52 +0000

    Updated configuration

--- odoo/dev.nix
+++ odoo/dev.nix
@@ -35,7 +35,11 @@
           ln -s /home/user/$WS_NAME/.idx/.data/odoo/odoo-bin .venv/bin/odoo-bin
           ln -s /usr/lib/libldap.so .venv/lib/libldap_r.so
           source .venv/bin/activate
+          sed -i '/^python-ldap==/d' .idx/.data/odoo/requirements.txt
           NIX_LDFLAGS="$NIX_LDFLAGS -L$VIRTUAL_ENV/lib" pip install -r .idx/.data/odoo/requirements.txt
+          python -m pip install urllib3 werkzeug num2words pytz passlib
+          mkdir -p /home/user/odoo/custom_addons
+          touch odoo.conf 
           odoo-bin --save --stop-after-init
           mv ../.odoorc odoo.conf
           sed -i                                                                 \
