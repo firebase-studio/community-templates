d2f98493b0115b7d021cc19a2cc2a3e7bb1d8051
richa.vijayvargiya <richa.vijayvargiya@cognizant.com>
2025-12-08 13:58:21 +0000

    dev

--- nativescript/dev.nix
+++ nativescript/dev.nix
@@ -1,43 +1,38 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://developers.google.com/idx/guides/customize-idx-env
+
 { pkgs, ... }: {
-  # Which nixpkgs channel to use.
-  channel = "stable-24.05"; # or "unstable"
-  # Use https://search.nixos.org/packages to find packages
+  channel = "stable-24.05";
   packages = [
     pkgs.nodejs_20
     pkgs.jdk17
+    pkgs.androidenv.androidSdk
+    pkgs.androidenv.platform-tools
+    pkgs.androidenv.build-tools
+    pkgs.androidenv.ndk
+    pkgs.nodejs_20.pkgs.pnpm
   ];
-  # Sets environment variables in the workspace
-  env = { };
+  env = {
+    ANDROID_HOME = "${pkgs.androidenv.androidSdk}";
+    JAVA_HOME = "${pkgs.jdk17}";
+  };
   idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
-    extensions = [ 
-        "nrwl.angular-console"
-        "esbenp.prettier-vscode"
-        "firsttris.vscode-jest-runner"
+    extensions = [
+      "nrwl.angular-console"
+      "esbenp.prettier-vscode"
+      "firsttris.vscode-jest-runner"
     ];
     workspace = {
-      # Runs when a workspace is first created with this `dev.nix` file
       onCreate = {
         install = ''
-          npm ci --prefer-offline --no-audit --no-progress --timing
-          yes | npx ns preview'';
-        # Open editors for the following files by default, if they exist:
+          npm install -g nativescript
+          ns create myapp --svelte
+          cd myapp
+          pnpm install
+        '';
         default.openFiles = [ "app/app.js" ];
       };
-      # To run something each time the workspace is (re)started, use the `onStart` hook
+      onStart = {
+        preview = "ns preview";
+      };
     };
-    # Enable previews and customize configuration
-    # previews = {
-    #   enable = true;
-    #   previews = {
-    #     web = {
-    #       command =
-    #         [ "ns" "preview" "--" "--port" "$PORT" "--hostname" "0.0.0.0" ];
-    #       manager = "web";
-    #     };
-    #   };
-    # };
   };
 }
